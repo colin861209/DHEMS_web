1aa40663160e0f9c5e2a98428db7b672ee132425
colinWang <colin861209@gmail.com>
2021-03-08 12:48:40 +0800

    [Module] Chart Style js
    	Content:
    	1. show chart with red dash line
    	2. set series function
    	3. show chart with pink area

---
+++ main_js/chart_style.js
@@ -0,0 +1,214 @@
+function show_chart_with_redDashLine(chart_info, chart_series_type, chart_series_name, chart_series_data, chart_series_stack, chart_series_yAxis, simulate_timeblock) {
+
+    //set all series data
+    var series_data = [],
+        len = Object.keys(chart_series_name).length,
+        i = 0;
+
+    for (i; i < (len); i++) {
+        series_data.push({
+            type: chart_series_type[i],
+            name: chart_series_name[i],
+            data: chart_series_data[i],
+            stack: chart_series_stack[i],
+            yAxis: chart_series_yAxis[i]
+        });
+    }
+    //set all chart data
+    var charts = Highcharts.chart(chart_info[0], {
+        title: {
+            text: chart_info[1],
+            style: {
+                fontWeight: 'bold',
+                fontSize: '24px'
+            }
+        },
+        subtitle: {
+            text: chart_info[2]
+        },
+        legend: {
+            itemStyle: {
+                fontWeight: 'bold',
+                fontSize: '18px'
+            }
+        },
+        xAxis: {
+            max: 95,
+            title: {
+                text: chart_info[3], style: {
+                    fontWeight: 'bold',
+                    fontSize: '16px'
+                }
+            },
+            categories: [],
+            plotLines: [{
+                color: 'red', // Color value
+                dashStyle: 'ShortDash', // Style of the plot line. Default to solid
+                value: simulate_timeblock, // Value of where the line will appear
+                width: 1, // Width of the line   
+            }
+            ]
+        },
+        yAxis: [{
+            min: 0,
+            title: {
+                text: chart_info[4], style: {
+                    fontWeight: 'bold',
+                    fontSize: '16px'
+                }
+            }
+        }, {
+            // min: 0,
+            // max: 4,
+            title: {
+                text: chart_info[5],
+                rotation: 270,
+                style: {
+                    fontWeight: 'bold',
+                    fontSize: '16px'
+                }
+            },
+            opposite: true
+        }]
+        ,
+        tooltip: {
+            //enabled: false
+            formatter: function () {
+                return '<b>' + this.x + '</b><br/>' +
+                    this.series.name + ': ' + this.y + '<br/>' +
+                    'Total: ' + this.point.stackTotal;
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: false
+                },
+                enableMouseTracking: false,
+                marker: {
+                    enabled: false
+                }
+            },
+            column: {
+                stacking: 'normal'
+            }
+        },
+        series: series_data
+    });
+
+}
+
+function set_series_function(multi, series_type, DATA, stack_class, yAxis_locate, chart_series_type, chart_series_name, chart_series_data, chart_series_stack, chart_series_yAxis) {
+    if (multi == 1) { var DATA_NUM = Object.keys(DATA).length; } //get data row num       
+    else { var DATA_NUM = 1; }//get data row num
+
+    for (var i = 0; i < DATA_NUM; i++) {
+        chart_series_type.push(series_type);
+        if (multi == 1) {
+            chart_series_name.push(parseInt(Object.keys(DATA)[i]) + 1)
+            chart_series_data.push(DATA[Object.keys(DATA)[i]]);
+        }
+        else {
+            chart_series_name.push(stack_class);  //same as stack name
+            chart_series_data.push(DATA);
+        }
+        chart_series_stack.push(stack_class);
+        chart_series_yAxis.push(yAxis_locate);
+    }
+}
+
+function show_chart_with_pinkArea(chart_info, chart_series_type, chart_series_name, chart_series_data, chart_series_stack, chart_series_yAxis, chart_upband, chart_lowband) {
+    //set all series data
+    var series_data = [];
+    var len = Object.keys(chart_series_name).length;
+    var i = 0;
+
+    for (i; i < (len); i++) {
+        series_data.push({
+            type: chart_series_type[i],
+            name: chart_series_name[i],
+            data: chart_series_data[i],
+            stack: chart_series_stack[i],
+            yAxis: chart_series_yAxis[i]
+        });
+    }
+    //set all chart data
+    var charts = Highcharts.chart(chart_info[0], {
+        title: {
+            text: chart_info[1]
+        },
+        subtitle: {
+            text: chart_info[2]
+        },
+        xAxis: {
+            plotBands: [{
+                color: 'pink', // Color value
+                from: chart_upband, // Start of the plot band
+                to: chart_lowband // End of the plot band
+            }],
+            max: 95,
+            title: { text: chart_info[3] },
+            categories: []
+        },
+        yAxis: [{
+            min: 0,
+            title: {
+                text: chart_info[4]
+            }
+        }, {
+            // min: -4,
+            // max: 4,   
+            title: {
+                text: chart_info[5]
+            },
+            opposite: true
+        }]
+        ,
+        tooltip: {
+            //enabled: false
+            formatter: function () {
+                return '<b>' + this.x + '</b><br/>' +
+                    this.series.name + ': ' + this.y + '<br/>' +
+                    'Total: ' + this.point.stackTotal;
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: false
+                },
+                enableMouseTracking: false,
+                marker: {
+                    enabled: false
+                }
+            },
+            column: {
+                stacking: 'normal'
+            }
+        },
+        series: series_data
+    });
+
+}
+
+function set_each_load_function(multi, series_type, DATA, ID, stack_class, yAxis_locate, chart_series_type, chart_series_name, chart_series_data, chart_series_stack, chart_series_yAxis) {
+    if (multi == 1) { var DATA_NUM = Object.keys(DATA).length; } //get data row num       
+    else { var DATA_NUM = 1; }//get data row num
+
+    for (var i = 0; i < DATA_NUM; i++) {
+        chart_series_type.push(series_type);
+        if (multi == 1) {
+            chart_series_name.push(ID[Object.keys(ID)[i]]);
+            chart_series_data.push(DATA[Object.keys(DATA)[i]]);
+        }
+        else {
+            chart_series_name.push(stack_class);  //same as stack name
+            chart_series_data.push(DATA);
+        }
+        chart_series_stack.push(stack_class);
+        chart_series_yAxis.push(yAxis_locate);
+    }
+
+}
+
+export {show_chart_with_redDashLine, set_series_function, show_chart_with_pinkArea, set_each_load_function};
