e023c6630a1531bebff71a4e71081a257488481f
ColinWang <colin861209@gmail.com>
2021-05-09 21:43:01 +0800

    [ADD] Flag table display and modify

---
+++ back_end/send_newFlag.php
@@ -0,0 +1,28 @@
+<?php
+require 'commonSQL_data.php';
+
+function updateSQL($conn, $table, $target_col, $target_value, $condition_col, $condition_value) {
+
+    $sql = "UPDATE `$table` SET `$target_col` = '$target_value' WHERE `$condition_col` = '$condition_value'";    
+    mysqli_query($conn, $sql);
+}
+
+$newFlag = $_POST['phpReceive'];
+
+for ($i=0; $i < count($newFlag['name']); $i++) { 
+    
+    updateSQL($conn, $newFlag['table'], "flag", $newFlag['flag'][$i], "variable_name", $newFlag['name'][$i]);
+    
+    // $verifySQL = "SELECT `flag` FROM `" .$newFlag['table'] ."` WHERE `flag` IS NOT NULL";
+    $flag = sqlFetchAssoc($conn, "SELECT `flag` FROM `" .$newFlag['table'] ."` WHERE `flag` IS NOT NULL", array("flag"));
+
+    if ($flag == $newFlag['flag'])
+        $status = "success";
+    else
+        $status = "something went wrong";
+}
+
+echo json_encode(array("status" => $status));
+
+mysqli_close($conn);
+?>
---
+++ main_js/flag_modify.js
@@ -0,0 +1,105 @@
+function flag_table(flag) {
+
+    var tableData = {
+
+        name: ["旗標名", "狀態"]
+    }
+
+    for (let nameNum = 0; nameNum < tableData.name.length; nameNum++) {
+
+        var th = document.createElement('th');
+        th.appendChild(document.createTextNode(tableData.name[nameNum]));
+        th.setAttribute("style", "text-align: center; color:black");
+        document.getElementById('flag_thead').appendChild(th);
+    }
+
+    for (let typeNum = 0; typeNum < flag[0].length; typeNum++) {
+
+        var tr = document.createElement('tr');
+        tr.setAttribute("style", "text-align: center; color:black; font-size: 20px");
+
+        for (let dataNum = 0; dataNum < flag.length; dataNum++) {
+
+            var td = document.createElement('td');
+            switch (dataNum) {
+                case 1:
+
+                    var input = document.createElement('input');
+                    input.setAttribute("type", "text");
+                    input.setAttribute("id", flag[0][typeNum]);
+                    input.setAttribute("style", "text-align: center; background-color: #ABFFFF;");
+                    input.setAttribute("size", "5");
+                    input.setAttribute("value", flag[dataNum][typeNum]);
+                    input.setAttribute("onchange", "flag_change()");
+                    td.appendChild(input);
+                    break;
+
+                default:
+                    td.appendChild(document.createTextNode(flag[dataNum][typeNum]));
+                    break;
+            }
+            tr.appendChild(td);
+        }
+        document.getElementById('flag_tbody').appendChild(tr);
+    }
+}
+
+function flag_change() {
+
+    document.getElementById('btn_flagModify').style.display = "block";
+}
+
+function sendNewFlag(element) {
+
+    var get_Flag = []
+    var new_flag = []
+
+    switch (element.name) {
+        case "LHEMS":
+            get_Flag = LHEMS_flag;
+            break;
+
+        case "GHEMS":
+            get_Flag = GHEMS_flag;
+            break;
+
+        default:
+            alert("Wrong button name in Html")
+            break;
+    }
+    var new_flagData = {
+
+        table: element.name + "_flag",
+        name: get_Flag[0],
+        flag: new_flag
+    }
+    for (let index = 0; index < get_Flag[0].length; index++) {
+
+        new_flag[index] = document.getElementById(get_Flag[0][index]).value
+    }
+    console.log(new_flagData)
+    $.ajax
+        ({
+            type: "POST",
+            url: "back_end/send_newFlag.php",
+            data: { phpReceive: new_flagData },
+            // contentType: "application/x-www-form-urlencoded",
+            // processData: true,
+            success: function (response) {
+
+                response = JSON.parse(response);
+                console.log(response)
+                if (response.status == "success") {
+                    
+                    alert("旗 標 修 改 完 成")
+                    location.reload("/loadFix.html")
+                }
+            }
+        });
+}
+
+$(document).ready(function () {
+    $("#flag_table").click(function () {
+        $("#flags").fadeToggle();
+    });
+});
