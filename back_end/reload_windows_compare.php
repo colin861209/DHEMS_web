80d14ab7e137b1f9391c1bb8f5dea5da5f6476cd
ColinWang <colin861209@gmail.com>
2022-04-25 02:40:48 +0800

    [PHP OOP] Reload Windows

--- back_end/reload_windows_compare.php
+++ back_end/reload_windows_compare.php
@@ -1,13 +1,14 @@
 <?php
-require 'commonSQL_data.php';
+require 'fetch_mysql.php';
 
+$obj = new SQLQuery();
+$target_DB = '';
 $compare_timeblock = $_POST['compare_timeblock'];
 
 if (strpos($compare_timeblock['page_name'], 'backup') !== false) { $target_DB = 'backup_BaseParameter'; }
 else { $target_DB = 'BaseParameter';}
 
-$new_timeblock = sqlFetchAssoc($conn, "SELECT `parameter_name`, `value` FROM `". $target_DB ."` WHERE `parameter_name` LIKE '%next_simulate_timeblock' ", array("parameter_name", "value"));
-mysqli_close($conn);
+$new_timeblock = $obj->sqlFetchAssoc("SELECT `parameter_name`, `value` FROM `". $target_DB ."` WHERE `parameter_name` LIKE '%next_simulate_timeblock' ", array("parameter_name", "value"));
 
 if ($compare_timeblock['local'] == $new_timeblock[1][array_search("next_simulate_timeblock", $new_timeblock[0], true)] && 
     $compare_timeblock['global'] == $new_timeblock[1][array_search("Global_next_simulate_timeblock", $new_timeblock[0], true)]) {
